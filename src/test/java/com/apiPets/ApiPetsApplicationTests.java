4ebd3d42faf9f71e9f05f8816e9b37fd4bf6ecef
unknown <willianpilar@hotmail.com>
2021-04-13 17:04:39 -0300

    Adicionando padrão de projeto

---
+++ src/test/java/com/apiPets/ApiPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiPets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
